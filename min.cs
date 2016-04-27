d9dbf600a3d4ccfadcd507974990cb4e526ff2e1
EastBrest <d90375@gmail.com>
2016-04-27 18:12:32 +0300

    1
    1

---
+++ min.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vodka =  new int[10];
+            int max = 0;
+            for(int i = 0;i<vodka.Length;i++)
+            {
+                vodka[i] = Convert.ToInt32(Console.ReadLine());
+                if (vodka[i] > max) //если введнное число с консоли больше предыдущего, то оно заменяется
+                {
+                    max = vodka[i];
+                }
+            }
+           
+            Console.WriteLine(max);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
