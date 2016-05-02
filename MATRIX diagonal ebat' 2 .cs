2bbfd14516f5a0b329bbc1d524c25459b621693e
EastBrest <d90375@gmail.com>
2016-05-02 18:48:53 +0300

    1
    1

---
+++ MATRIX diagonal ebat' 2 .cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] arr = new int[n, m];
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            int sumGD = 0;
+            int sumLGD = 0;
+            int sumHGD = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (j == i)
+                    {
+                        sumGD += arr[i, j];
+                    }
+                    if (j > i)
+                    {
+                        sumHGD += arr[i, j];
+                    }
+                    if (j < i)
+                    {
+                        sumLGD += arr[i, j];
+                    }
+
+
+                }
+            }
+
+            int sumPD = 0;
+            int sumLPD = 0;
+            int sumHPD = 0;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+
+                    if (i == (m - j - 1))
+                    {
+                        sumPD += arr[i,j];
+                    }
+                    if (i > (m - 1 - j))
+                    {
+                        sumLPD += arr[i,j];
+                    }
+                    if (i < (m - 1 - j))
+                    {
+
+                        sumHPD += arr[i,j];
+                    }
+                }
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write(arr[i, j] + "\t");
+                }
+                Console.WriteLine("\n");
+            }
+
+            Console.WriteLine("сумма главной диагонали " + sumGD);
+            Console.WriteLine("сумма под главной диагональю " + sumLGD);
+            Console.WriteLine("сумма над главной диагональнью " + sumHGD);
+            Console.WriteLine("сумма  побочной диагонали " + sumPD);
+            Console.WriteLine("сумма под побочной диагональю " + sumLPD);
+            Console.WriteLine("сумма над побочной диагональю " + sumHPD);
+
+
+            Console.ReadKey();
+        }
+    }
+}
