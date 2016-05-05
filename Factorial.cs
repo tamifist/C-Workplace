ff44c0d8cf97847340191a9c1ea1de4ef04172b5
EastBrest <d90375@gmail.com>
2016-05-05 20:20:22 +0300

    1
    1

---
+++ Factorial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+
+    class Program
+    {
+        
+        static int F(int n)
+        {
+            inn(n);
+            int res;
+            if (n == 0)
+            {
+                res = 1;
+            }
+            if (n == 1)
+            {
+
+                res = 1;
+            }
+
+            else
+
+            {
+                
+                res = n * F(n - 1);
+                resRes(res);
+
+            }
+            outt(n);
+            return res;
+        }
+        public static void resRes(int x)
+        {
+            spaces();
+            Console.WriteLine( " === "  + x);
+        }
+        public static void inn(int x)
+        {
+            spaces();
+            Console.WriteLine("(" + x + ")->");
+            
+        }
+        public static void outt(int x)
+        {
+            spaces();
+            Console.WriteLine("(" + x + ")<-");
+        }
+        private static void spaces()
+        {
+            
+                Console.Write("  *   ");
+        }
+        static void Main()
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("факториал числа :{0} = {1}", n, F(n));
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
---
+++ papki.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication20
+{
+    class Program
+    {
+        public static string dirD = "D:\\";
+        static void Main(string[] args)
+        {
+            foreach (string i in SearchDirectory(dirD))
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+        static string[] SearchDirectory(string patch)
+        {
+            
+            string[] ResultSearch = Directory.GetDirectories(patch);
+            
+            return ResultSearch;
+        }
+        
+        
+    }
+
+    }
+
