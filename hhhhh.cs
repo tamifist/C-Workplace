9e7c5b98dedbd70b89d6f9209faa9d871ec64a98
EastBrest <d90375@gmail.com>
2016-05-05 20:58:08 +0300

    1
    1

---
+++ hhhhh.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication21
+{
+    class Program
+    {
+        static string dir = "D:\\";
+        static string[] dirArray = Directory.GetDirectories(dir);
+        static void Main(string[] args)
+        {
+            searchDir(dirArray,dirArray.Length);
+        }
+
+        static void searchDir (string[] dirArray, int count)
+        {
+            if(count > 0)
+            {
+
+                searchDir(dirArray, count - 1);
+            }
+        }
+
+
+    }
+}
